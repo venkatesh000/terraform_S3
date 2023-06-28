8ffa71134a4e81cacca0b5eb737b56262670aed0
venkatesh000 <pallivenkatesh5@gmail.com>
2023-06-28 15:43:58 +0530

    sample test

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
   access_key = "my-access-key"   #enter here access_key
   secret_key = "my-secret-key"   #enter here secret _key
 }
-resource "aws_s3_bucket" "bucket" {
+resource "aws_s3_bucket" "bucket8364" {
   bucket = "Create S3 bucket & Upload"
 
   tags = {
@@ -13,7 +13,7 @@ resource "aws_s3_bucket" "bucket" {
 }
 
 resource "aws_s3_bucket_object" "object" {
-  bucket = "aws_s3_bucket.bucket.id"   #enter here source name(line6) with .id
+  bucket = "aws_s3_bucket.bucket8364.id"   #enter here source name(line6) with .id
   key    = "hello.txt"     #file name
   source = "C:\Users\PalliVenkatesh\Documents\terraform_vpc\hello.txt"  #location of the file
 }
