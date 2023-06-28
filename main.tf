4d15012f5443859c581d6e523b6dcb01b26e3e3a
venkatesh000 <pallivenkatesh5@gmail.com>
2023-06-28 12:54:53 +0530

    latest S3 code

---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "my-access-key"   #enter here access_key
+  secret_key = "my-secret-key"   #enter here secret _key
+}
+resource "aws_s3_bucket" "bucket" {
+  bucket = "Create S3 bucket & Upload"
+
+  tags = {
+    Name = "My bucket"
+  }
+    
+}
+
+resource "aws_s3_bucket_object" "object" {
+  bucket = "aws_s3_bucket.bucket.id"   #enter here source name(line6) with .id
+  key    = "hello.txt"     #file name
+  source = "C:\Users\PalliVenkatesh\Documents\terraform_vpc\hello.txt"  #location of the file
+}
